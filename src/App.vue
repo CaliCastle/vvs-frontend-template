cbd96f74c8114d8b0d9322c250a77f61c4db12bd
Cali Castle <cali@calicastle.com>
2019-04-29 23:08:24 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+body,
+html {
+  margin: 0;
+}
+#app {
+  font-family: "Roboto", -apple-system, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background: #883f34;
+  color: #f6f6f6;
+  min-height: 100vh;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #fff;
+    &.router-link-exact-active {
+      color: #c9c565;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="VVS logo" src="../assets/logo.png" style="width: 45vw" />
+    <HelloWorld msg="VVS Rocks" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
